fe8bed850ee07ff62c4bef0b46c4cfc2b2b774c2
coderakshayagarwal <coder.akshayagarwal@gmail.com>
2024-11-21 23:38:45 +0530

    02counter - hook - useState

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(15);
+  function addValue() { 
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  }
+
+  function removeValue() {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  }
+
+  return (
+    <>
+      <h1>PumbCode</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue} >Add Value {counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove Value {counter}</button>
+      <p>
+        footer: {counter}
+      </p>
+    </>
+  )
+}
+
+export default App
